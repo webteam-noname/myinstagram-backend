52a39d9438658075a9016d946a55c97f27351166
OiKimiO <etkim02@naver.com>
2023-05-29 08:04:13 +0900

    Remove #6 - File 기능제거

--- src/main/java/com/my/instagram/domains/file/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.my.instagram.domains.file.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class FileService {
-}

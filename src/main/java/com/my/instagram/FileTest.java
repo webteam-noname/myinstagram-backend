3ce85d20646b1f0a4a0e83cbc4a0b3e726e5774d
OiKimiO <etkim02@naver.com>
2023-05-30 20:28:59 +0900

    Add #9 - 프로필 구현 (테스트, 리팩터링 필요)

--- src/main/java/com/my/instagram/FileTest.java
+++
@@ -1,8 +0,0 @@
-package com.my.instagram;
-
-public class FileTest {
-    public static void main(String[] args) {
-        String str = "abc.txt";
-        System.out.println(str.substring(str.lastIndexOf(".")+1));
-    }
-}
